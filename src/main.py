f7e6a7e10d5252d7a18fe3411a97848db09db6ea
kainatabdulrasheed <259586191+kainatabdulrasheed@users.noreply.github.com>
2026-09-06 01:13:40 +0500

    Add progress data persistence and streak tracking

--- src/main.py
+++ src/main.py
@@ -1,20 +1,39 @@
 import tkinter as tk
+from tracker import load_data, save_data
 
+# Load saved data
+data = load_data()
+
+current_day = data["current_day"]
+streak = data["streak"]
+best_streak = data["best_streak"]
 
 # Main window
 window = tk.Tk()
 window.title("100-Day DSA Tracker")
 window.geometry("400x500")
 
-current_day = 0
-
-
 def complete_today():
     global current_day
+    global streak
+    global best_streak
+
     if current_day < 100:
         current_day += 1
+        streak += 1
+
+        if streak > best_streak:
+            best_streak = streak
+
+        data["current_day"] = current_day
+        data["streak"] = streak
+        data["best_streak"] = best_streak
+
+        save_data(data)
+
         day_label.config(text=f"DAY {current_day:02d} / 100")
-        # complete_button.config(state="disabled")
+        streak_label.config(text=f"🔥 STREAK: {streak} DAYS")
+        best_label.config(text=f"🏆 BEST: {best_streak} DAYS")
 # Title
 title_label = tk.Label(
     window,
@@ -27,7 +46,7 @@ title_label.pack(pady=20)
 # Day counter
 day_label = tk.Label(
     window,
-    text="DAY 0 / 100",
+    text=f"DAY {current_day:02d} / 100",
     font=("Arial", 18)
 )
 day_label.pack(pady=10)
@@ -36,7 +55,7 @@ day_label.pack(pady=10)
 # Current streak
 streak_label = tk.Label(
     window,
-    text="🔥 STREAK: 0 DAYS",
+    text=f"🔥 STREAK: {streak} DAYS",
     font=("Arial", 14)
 )
 streak_label.pack(pady=5)
@@ -45,7 +64,7 @@ streak_label.pack(pady=5)
 # Best streak
 best_label = tk.Label(
     window,
-    text="🏆 BEST: 0 DAYS",
+    text=f"🏆 BEST: {best_streak} DAYS",
     font=("Arial", 14)
 )
 best_label.pack(pady=5)
---
+++ src/tracker.py
@@ -0,0 +1,13 @@
+import json
+
+DATA_FILE = "src/data.json"
+
+
+def load_data():
+    with open(DATA_FILE, "r") as file:
+        return json.load(file)
+
+
+def save_data(data):
+    with open(DATA_FILE, "w") as file:
+        json.dump(data, file, indent=4)
